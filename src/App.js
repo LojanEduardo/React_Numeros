2c40118d0c1c664a8243101e518f2f70631174a7
Lojan Eduardo <lojanpetrovich@gmail.com>
2021-12-22 23:45:52 -0300

    Commit Inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{number}</h1>
+      <input type="text" 
+        name="number" 
+        value={number} 
+        onChange={(event) => setNumber(event.target.value)} 
+      />
+      <button className = "botão" onClick={()=>setNumber(number+1)}>+</button>
+    </>      
   );
 }
 
